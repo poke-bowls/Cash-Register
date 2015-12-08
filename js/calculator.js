285df03b53b4c388fe371747fd79fe4a7e4b7a0b
Micah Koki <micah.koki@gmail.com>
2015-12-08 12:33:13 -0800

    Finished Register

--- js/calculator.js
+++ js/calculator.js
@@ -1,58 +1,125 @@
 var calculator = function() {
-  //Public variables
-  var memory = 0,
-  total = 0,
-  equalsPressed = false,
-  operator,
-  operatorSet = false,
+  //Private variables
+  var eqInput,
+      currNumberInput,
+      equalsPressed = false,
+      operator,
+      operatorSet = false,
+      memory = null,
 
-  recallMemory = function () {
-    return memory;
-  },
-
-  memoFun = function(input) {
-    if (memory === 0) {
-      memory = input;
-    } else {
-      memory = memory.toString() + input.toString();
-      memory = Number(memory);
-    }
-  },
-
-  setOperator = function(newOperator) {
-    if (newOperator == '=') {
-      equalsPressed = true;
-    }
-  },
-
-  load = function (x) {
-    total = x;
-    return total;
+  start = function(equals, currNumber) {
+    eqInput = equals;
+    currNumberInput = currNumber;
   },
 
   add = function (x, y) {
-    return x + y;
+    var sum = x + y;
+    return sum.toFixed(2);
   },
 
   subtract = function (x, y) {
-    return x - y;
+    var difference = x - y;
+    return difference.toFixed(2);
   },
 
   multiply = function (x, y) {
-    return x * y;
+    var product = x * y;
+    return product.toFixed(2);
   },
 
   divide = function (x, y) {
     if (y === 0) {
       alert("Error: Can't divide by 0");
     }
-    return x / y;
+    var quotient = x / y;
+    return quotient.toFixed(2);
   },
 
+  setValue = function (value) {
+    currNumberInput.innerHTML = value;
+  },
+
+  setEquation = function (value) {
+    eqInput.innerHTML = value;
+  },
+
+  clearNumbers = function () {
+    memory = null;
+    equalsPressed = false;
+    operatorSet = false;
+    setValue('0');
+    setEquation('');
+  },
+
+  clickNumber = function (event) {
+    var button = (event.target) ? event.target : event.srcElement;
+    if (operatorSet === true || currNumberInput.innerHTML === '0') {
+      setValue('');
+      operatorSet = false;
+    }
+    setValue(currNumberInput.innerHTML + button.innerHTML);
+    setEquation(eqInput.innerHTML + button.innerHTML);
+  },
+
+  //Function should handle if operator was = or +, -, *, /
+  setOperator = function(newOperator) {
+    if (newOperator == '=') {
+      equalsPressed = true;
+      calculate();
+      setEquation('');
+      return;
+    }
+    //If = pressed then a new operator (+, -, *, /), hando dat
+    if (!equalsPressed) {
+      //set the input (button pressed) to the var operator
+      operator = newOperator;
+      //make sure equals pressed is set to false
+      equalsPressed = false;
+      //run our calculate function to compute the new value with the set operation
+      calculate();
+      //note that the operator is now set
+      operatorSet = true;
+      //parse the string located in calculator, returning a number. Save this to the memory var
+      memory = parseFloat(currNumberInput.innerHTML);
+      /*If the string in the equation part of calculator is empty, save the current Number
+      (stored in memory) and operator to the var eqText.  If not, add the operator to the
+      string in the equation part of calculator and store it in eqText. */
+      var eqText = (eqInput.innerHTML === '') ? memory + ' ' + operator + ' ' : eqInput.innerHTML + ' ' + operator + ' ';
+      //set eqText to the equation part of the calculator
+      setEquation(eqText);
+    }
+  },
+
+  //Calculate depending on what operator was passed +, -, *, /
   calculate = function() {
+    if (!operator || memory === null) {
+      return;
+    }
 
+    var newValue = 0,
+        currNumber = parseFloat(currNumberInput.innerHTML);
+    switch(operator){
+      case '+':
+        newValue = add(memory, currNumber);
+        break;
+      case '-':
+        newValue = subtract(memory, currNumber);
+        break;
+      case '*':
+        newValue = multiply(memory, currNumber);
+        break;
+      case '/':
+        newValue = divide(memory, currNumber);
+        break;
+    }
+    setValue(newValue);
+    memory = newValue;
   };
+  //Public variables
   return {
-
+    start: start,
+    clickNumber: clickNumber,
+    setOperator: setOperator,
+    clearNumbers: clearNumbers
   };
-} ();
+}();
--- js/cash_register.js
+++ js/cash_register.js
@@ -1,91 +1,37 @@
-document.getElementById("seven").addEventListener("click", inputSeven);
-document.getElementById("eight").addEventListener("click", inputEight);
-document.getElementById("nine").addEventListener("click", inputNine);
-document.getElementById("divide").addEventListener("click", divideFunc);
-document.getElementById("clear").addEventListener("click", clearMemory);
-document.getElementById("four").addEventListener("click", inputFour);
-document.getElementById("five").addEventListener("click", inputFive);
-document.getElementById("six").addEventListener("click", inputSix);
-document.getElementById("multiply").addEventListener("click", multiplyFunc);
-document.getElementById("getBalance").addEventListener("click", getTotal);
-document.getElementById("one").addEventListener("click", inputOne);
-document.getElementById("two").addEventListener("click", inputTwo);
-document.getElementById("three").addEventListener("click", inputThree);
-document.getElementById("subtract").addEventListener("click", subtractFunc);
-document.getElementById("depositCash").addEventListener("click", depositCash);
-document.getElementById("zero").addEventListener("click", inputZero);
-document.getElementById("doubleZero").addEventListener("click", inputDblZero);
-document.getElementById("decimal").addEventListener("click", inputDecimal);
-document.getElementById("addition").addEventListener("click", addFunc);
+var register = function() {
+  //Private variables
+  var balance = 0,
 
-//Number input functions
-function inputSeven() {
-  Calculator.memoFunc(7);
-  document.getElementById("display").innerHTML = Calculator.recallMemory();
-}
+  start = function(equals, currNumber) {
+    eqInput = equals;
+    currNumberInput = currNumber;
+  },
 
-function inputEight () {
-  document.getElementById("display").innerHTML = Calculator.recallMemory();
-}
+  getBalance = function() {
+    currNumberInput.innerHTML = "$ " + balance.toFixed(2);
+    eqInput.innerHTML = '';
+  },
 
-function inputNine() {
-  document.getElementById("display").innerHTML = Calculator.recallMemory();
-}
+  deposit = function() {
+    var currNumber = parseFloat(currNumberInput.innerHTML);
+    balance += currNumber;
+    calculator.clearNumbers();
+  },
 
-function inputFour() {
-  document.getElementById("display").innerHTML = Calculator.recallMemory();
-}
-
-function inputFive() {
-  document.getElementById("display").innerHTML = Calculator.recallMemory();
-}
-
-function inputSix() {
-  document.getElementById("display").innerHTML = Calculator.recallMemory();
-}
-
-function inputOne() {
-  document.getElementById("display").innerHTML = Calculator.recallMemory();
-}
-
-function inputTwo() {
-  document.getElementById("display").innerHTML = Calculator.recallMemory();
-}
-
-function inputThree() {
-  document.getElementById("display").innerHTML = Calculator.recallMemory();
-}
-
-function inputZero() {
-  document.getElementById("display").innerHTML = Calculator.recallMemory();
-}
-
-function inputDblZero() {
-  document.getElementById("display").innerHTML = Calculator.recallMemory();
-}
-
-//Operation functions
-
-function divideFunc() {
-  document.getElementById("display").innerHTML = Calculator.recallMemory();
-}
-
-function clearMemory() {
-  document.getElementById("display").innerHTML = Calculator.recallMemory();
-}
-
-function multiplyFunc() {
-  document.getElementById("display").innerHTML = Calculator.recallMemory();
-}
-
-function getTotal() {
-  document.getElementById("display").innerHTML = Calculator.recallMemory();
-}
-
-function subtractFunc() {
-  document.getElementById("display").innerHTML = Calculator.recallMemory();
-}
-
-function depositCash() {
-  document.getElementById("display").innerHTML = Calculator.recallMemory();
-}
+  withdraw = function() {
+    var currNumber = parseFloat(currNumberInput.innerHTML);
+    if((balance - currNumber) >= 0){
+    balance -= currNumber;
+    } else {
+      currNumberInput.innerHTML = "Insufficient Funds";
+    }
+    calculator.clearNumbers();
+  };
+  //Public variables
+  return {
+    start: start,
+    getBalance: getBalance,
+    withdraw: withdraw,
+    deposit: deposit
+  };
+}();
